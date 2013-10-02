93f046cf5c2cb1cc536da43500cd2614a34144ee
guest01 <guest01@10.1.49.153>
2013-10-02 11:56:42 -0300

    Classe criada pelo felipe "Hello Word" Aspira boo

--- src/Gasolina.java
+++ src/Gasolina.java
@@ -3,18 +3,22 @@ import javax.swing.JOptionPane;
 
 public class Gasolina {
     public static void main(String[] args) {
-        double gasolina;
-        double litro;
+        String gasolina;
+        String litro;
         int soma = 1;
+        double gasolinaS;
         
-        gasolina = Double.parseDouble(JOptionPane.showInputDialog("Informe o valor da gasolina:"));
-        litro = Double.parseDouble(JOptionPane.showInputDialog("Informe a quantidade de litros que você quer abastecer:"));
+        gasolina = JOptionPane.showInputDialog("Informe o valor da gasolina:");
+        litro = JOptionPane.showInputDialog("Informe a quantidade de litros que você quer abastecer:");
+        
+        gasolina.replaceAll( "," , "." );  
+        gasolinaS = Double.parseDouble(gasolina); 
         
         while(soma > 0)
-        if(gasolina == 10){
+        if(gasolinaS == 10){
             JOptionPane.showMessageDialog(null,"Você vai andar 200,00 R$");
             System.out.println("Você vai andar 200,00 R$");
-        }else if (gasolina < 10 && gasolina > 10){
+        }else if (gasolinaS < 10 && gasolinaS > 10){
             JOptionPane.showMessageDialog(null,"AA pegadinha do malandro!!!!!");
             System.out.println("AA pegadinha do malandro!!!!!");
         }
